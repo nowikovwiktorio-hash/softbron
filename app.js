2a0fe13b97d9de728854859d6eda9205f2520f80
nowikovwiktorio-hash <nowikovwiktorio@gmail.com>
2025-12-22 03:43:12 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const today = new Date();
-    const d = String(today.getDate()).padStart(2, '0');
-    const m = String(today.getMonth() + 1).padStart(2, '0');
-    const y = today.getFullYear();
-    document.querySelectorAll('#date').forEach(el => el.textContent = `${d}.${m}.${y}`);
-});
